71952a86d8fbdd9f86f62d30c18dc408f2629f72
MaciekLPL <maciekl2299@gmail.com>
2022-05-13 11:13:03 +0200

    random parking

--- Assets/Scripts/ParkAgent.cs
+++ Assets/Scripts/ParkAgent.cs
@@ -4,78 +4,73 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
-public class ParkAgent : Agent
-{
+public class ParkAgent : Agent {
     // Start is called before the first frame update
-     [SerializeField] private Transform targetTransform;
-     [SerializeField] private SimpleCarController carController;
-     [SerializeField] private Parking parkingSpot;
-     private float _horizontalInput = 0f; 
-     private float _verticalInput = 0f;
-     private float distance = 0.0f;
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private SimpleCarController carController;
+    [SerializeField] private Parking parkingSpot;
+    private float _horizontalInput = 0f;
+    private float _verticalInput = 0f;
+    private float distance = 0.0f;
 
-     public override void OnEpisodeBegin()
-     {    AddReward(-1f);
-          transform.position = new Vector3(-5.1f, 0f, -7.32f);
-          transform.eulerAngles = new Vector3(0f,90f,0f);
-          parkingSpot.respawn();
-          distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - targetTransform.position.x,2)+Mathf.Pow(transform.position.y - targetTransform.position.y,2)+Mathf.Pow(transform.position.z - targetTransform.position.z,2));
-     }
-     public override void CollectObservations(VectorSensor sensor)
-     {
-          //sensor.AddObservation(transform.position);
-          //sensor.AddObservation(targetTransform.position);
-          sensor.AddObservation(Vector3.Distance(transform.position, targetTransform.position));
-          sensor.AddObservation((targetTransform.position - transform.position).normalized);
-          sensor.AddObservation(transform.forward);
-     }
-     public override void OnActionReceived(ActionBuffers actions)
-     {     
-   
-          switch(actions.DiscreteActions[0]){
-               case 0: _horizontalInput = 0f; break;
-               case 1: _horizontalInput = 1f; break; 
-               case 2: _horizontalInput = -1f; break; 
-          }
-          switch(actions.DiscreteActions[1]){
-               case 0: _verticalInput = 0f; break; 
-               case 1: _verticalInput = 1f; break; 
-               case 2: _verticalInput = -1f; break; 
-          }
+    public override void OnEpisodeBegin() {
+        transform.position = new Vector3(Random.Range(-10, -5), 0, Random.Range(-10, -5));
+        transform.eulerAngles = new Vector3(0f, Random.Range(30, 60), 0f);
+        parkingSpot.respawn();
+        distance = Vector3.Distance(transform.position, targetTransform.position);
+    }
+    public override void CollectObservations(VectorSensor sensor) {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+        sensor.AddObservation(Vector3.Distance(transform.position, targetTransform.position));
+        sensor.AddObservation((targetTransform.position - transform.position).normalized);
+        sensor.AddObservation(transform.forward);
+    }
+    public override void OnActionReceived(ActionBuffers actions) {
 
-          carController.setInput(_horizontalInput, _verticalInput);
-          if(transform.eulerAngles.z >= 45f && transform.eulerAngles.z <= 315f)
-          {
-               Debug.Log("Wywalil sie:" +transform.eulerAngles.z);
-               AddReward(-1f);
-               EndEpisode();
-          }
-          float tmp = Mathf.Sqrt(Mathf.Pow(transform.position.x - targetTransform.position.x,2)+Mathf.Pow(transform.position.y - targetTransform.position.y,2)+Mathf.Pow(transform.position.z - targetTransform.position.z,2));
-          if(tmp<distance)
-               AddReward(0.01f);
-          distance = tmp;
+        switch (actions.DiscreteActions[0]) {
+            case 0: _horizontalInput = 0f; break;
+            case 1: _horizontalInput = 1f; break;
+            case 2: _horizontalInput = -1f; break;
+        }
+        switch (actions.DiscreteActions[1]) {
+            case 0: _verticalInput = 0f; break;
+            case 1: _verticalInput = 1f; break;
+            case 2: _verticalInput = -1f; break;
+        }
 
-   }
+        carController.setInput(_horizontalInput, _verticalInput);
+        
+        if (transform.eulerAngles.z >= 45f && transform.eulerAngles.z <= 315f) {
+            Debug.Log("Wywalil sie:" + transform.eulerAngles.z);
+            AddReward(-1f);
+            EndEpisode();
+        }
 
-     private void OnTriggerEnter(Collider collider)
-     {
-          if(collider.gameObject.TryGetComponent<Wall>(out Wall wall)){
-               AddReward(-1f);
-               Debug.Log("Przypierdolil dzwona");
-               EndEpisode();
-          }
-          else if(collider.gameObject.TryGetComponent<Parking>(out Parking parking)){
-               Debug.Log("GG");
-               SetReward(1f);
-               EndEpisode();
+        float tmp = Vector3.Distance(transform.position, targetTransform.position);
+        if (tmp < distance)
+            AddReward(1f / MaxStep);
+        else
+            AddReward(-1f / MaxStep);
+        distance = tmp;
 
-          }
-          else
-          {
-               AddReward(-0.1f);
-               Debug.Log("Uderzyl w cos");
-          }
-     }
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        
+        if (collider.gameObject.TryGetComponent<Parking>(out Parking parking)) {
+            Debug.Log("Parking");
+            AddReward(1f);
+            EndEpisode();
+        } else {
+            Debug.Log("Kolizja");
+            AddReward(-1f);
+            EndEpisode();
+        }
+    }
+
+
+
 
 
 }
