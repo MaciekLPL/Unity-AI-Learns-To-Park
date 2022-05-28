c637e1c7f56cbb6411b69d83ff7836bc1c5c15d7
Szesny32 <bartoszszczesny.studia@gmail.com>
2022-05-29 00:36:58 +0200

    Agent training: 40 million iterations

---
+++ version A/Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+
+    float yPos = 0.5f;
+    float[] xPos = {-5.05f, 2.245f};
+    float[] zPos = { 10.1f, 8.13f, 6.14f, 4.14f, 2.14f, 0.15f, -1.88f, -3.89f, -5.87f, -7.9f, -9.87f};
+    [SerializeField] private GameObject prefab_car_m1;
+    [SerializeField] private GameObject prefab_car_m2;
+    [SerializeField] private GameObject prefab_car_m3;
+    [SerializeField] private GameObject prefab_car_m4;
+    [SerializeField] private GameObject prefab_car_m5;
+    [SerializeField] private Parking parkingSpot;
+    private GameObject[] actors = new GameObject[16];
+    void Start()
+    {
+        //respawn();
+    }
+    
+    void Update()
+    {
+    
+    }
+
+
+    public void respawn()
+    {
+        for(uint i = 0; i<16; i++)
+        {
+            if(actors[i])
+                Destroy(actors[i]);
+        }
+
+
+       // 5% szans na parking 19% na szamochód
+        for(uint i = 0; i<5; i++)
+        {
+            int dice = Random.Range(1, 100);
+            int type = Random.Range(1, 10);
+            float angle = (type<6) ? 90.0f : -90.0f;
+            if(dice < 6);
+            else if(dice < 25)
+                actors[i]= Instantiate(prefab_car_m1, transform.position + new Vector3(xPos[0],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else if(dice < 44)
+                actors[i]= Instantiate(prefab_car_m2, transform.position + new Vector3(xPos[0],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else if(dice < 63)
+                actors[i]= Instantiate(prefab_car_m3, transform.position + new Vector3(xPos[0],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else if(dice < 82)
+                actors[i]= Instantiate(prefab_car_m4, transform.position + new Vector3(xPos[0],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else 
+                actors[i]= Instantiate(prefab_car_m5, transform.position + new Vector3(xPos[0],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+        }
+
+        for(uint i = 0; i<11; i++)
+        {
+            int dice = Random.Range(1, 100);
+            int type = Random.Range(1, 10);
+            float angle = (type<6) ? 90.0f : -90.0f;
+            if(dice < 6);
+            else if(dice < 25)
+                actors[5+i]= Instantiate(prefab_car_m1, transform.position + new Vector3(xPos[1],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else if(dice < 44)
+                actors[5+i]= Instantiate(prefab_car_m2, transform.position + new Vector3(xPos[1],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else if(dice < 63)
+                actors[5+i]= Instantiate(prefab_car_m3, transform.position + new Vector3(xPos[1],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);   
+            else if(dice < 82)
+                actors[5+i]= Instantiate(prefab_car_m4, transform.position + new Vector3(xPos[1],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);
+            else 
+                actors[5+i]= Instantiate(prefab_car_m5, transform.position + new Vector3(xPos[1],0f,zPos[i]), Quaternion.Euler(0, angle, 0), this.transform);  
+        }
+
+        int n = Random.Range(0, 15);
+        if(actors[n])
+                Destroy(actors[n]);
+         if(n<5)
+            parkingSpot.respawn(transform.position + new Vector3(xPos[0], 0f, zPos[n]));
+         else       
+            parkingSpot.respawn(transform.position + new Vector3(xPos[1], 0f, zPos[n-5]));
+    }
+}
---
+++ version A/Assets/Scripts/ParkAgent.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+public class ParkAgent : Agent {
+    // Start is called before the first frame update
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private SimpleCarController carController;
+    [SerializeField] private EnvironmentManager environmentManager;
+    [SerializeField] private Transform plansza;
+    [SerializeField] private GameObject colorTile;
+    private float _horizontalInput = 0f;
+    private float _verticalInput = 0f;
+    private float distance = 0.0f;
+    private bool touchParkingBefore = false;
+    [SerializeField] private bool debugMe = false;
+
+    public override void OnEpisodeBegin() {
+        touchParkingBefore =false;
+        carController.reset();
+        transform.position = plansza.position + new Vector3(Random.Range(-10, -5), 0, Random.Range(-10, -5));
+        transform.eulerAngles = new Vector3(0f, Random.Range(30, 60), 0f);
+        environmentManager.respawn();
+        distance = Vector3.Distance(transform.position, targetTransform.position);
+
+    }
+    public override void CollectObservations(VectorSensor sensor) {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+        sensor.AddObservation(Vector3.Distance(transform.localPosition, targetTransform.localPosition));
+        sensor.AddObservation((targetTransform.localPosition - transform.localPosition).normalized);
+        sensor.AddObservation(transform.forward);
+        sensor.AddObservation(carController.getVelocity());
+    }
+    public override void OnActionReceived(ActionBuffers actions) {
+
+        switch (actions.DiscreteActions[0]) {
+            case 0: _horizontalInput = 0f; break;
+            case 1: _horizontalInput = 1f; break;
+            case 2: _horizontalInput = -1f; break;
+        }
+        switch (actions.DiscreteActions[1]) {
+            case 0: _verticalInput = 0f; break;
+            case 1: _verticalInput = 1f; break;
+            case 2: _verticalInput = -1f; break;
+        }
+
+        carController.setInput(_horizontalInput, _verticalInput);
+        
+        if (transform.eulerAngles.z >= 45f && transform.eulerAngles.z <= 315f) {
+
+            AddReward(-1f);
+                    colorTile.GetComponent<Renderer>().material.color = Color.yellow;
+            EndEpisode();
+        }
+
+        float tmp = Vector3.Distance(transform.position, targetTransform.position);
+        if (tmp < distance)
+            AddReward(1f / MaxStep);
+        else if(tmp < 0.5f) {}
+        else
+            AddReward(-1f / MaxStep);
+        distance = tmp;
+
+        float direction = Mathf.Abs(Vector3.Dot(transform.forward, targetTransform.forward));
+        float angle = 90f - 90f*direction; 
+
+        
+        // if(debugMe)
+        //     Debug.Log($"{Mathf.Abs(carController.getVelocity())}v : {tmp}d");
+
+        float maxAngle = 3f;
+        if (Mathf.Abs(carController.getVelocity()) < 0.05f && tmp < 0.1f && angle < maxAngle) 
+        
+        {
+            float angleReward =  1f- (direction/maxAngle)*0.5f;
+
+            float distanceReward = (1f - ((float)StepCount/(float)MaxStep));
+            float reward =  distanceReward + angleReward;
+            
+            Debug.Log("Sukces");
+            AddReward(reward);
+                    colorTile.GetComponent<Renderer>().material.color = Color.green;
+            EndEpisode();
+
+        }
+        if(StepCount>=2000 &&(colorTile.GetComponent<Renderer>().material.color == Color.red ||touchParkingBefore==false))
+        colorTile.GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut) {
+
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Mathf.CeilToInt(Input.GetAxis("Horizontal"));
+        discreteActions[1] = Mathf.CeilToInt(Input.GetAxis("Vertical"));
+
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        
+        if (collider.gameObject.TryGetComponent<Parking>(out Parking parking)) {
+            if(touchParkingBefore==false)
+            {
+                Debug.Log("Parking");
+                touchParkingBefore=true;
+                AddReward(1f);
+                colorTile.GetComponent<Renderer>().material.color = Color.magenta;
+            }
+            
+        } else {
+            Debug.Log("Kolizja");
+            colorTile.GetComponent<Renderer>().material.color = Color.red;
+            AddReward(-1f);
+            EndEpisode();
+        }
+    }
+}
